3faca6e04b11444033d16b2eb6baabb9d8be5f23
bloosterone <xldiegoo@gmail.com>
2023-12-01 11:10:35 -0300

    se modifican colores e index

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const typed = new Typed('#typed', {
     stringsElement: '#test',// ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 2, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	typeSpeed: 40, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 40, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
 	loopCount: 5, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
@@ -25,13 +25,13 @@ const typed = new Typed('#typed', {
 	
 	// Ajusta la posición según el margen que has agregado
 	
-	if (topPosition<= 350 &&  topPosition>= -896) {
+	if (topPosition<= 350 &&  topPosition>= -1766) {
 
-		body.style = 'background-color: #cb6df0;'
+		body.style = 'background-color: #ed1c5b;'
 
 
 	}
-	else if(topPosition<= -941 ){
+	else if(topPosition<= -1766 && topPosition>= -4000 ){
 		body.style = 'background-color: #fff;'
 	}
 	else{
